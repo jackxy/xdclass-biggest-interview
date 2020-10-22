6076cf382673d130b3dc6604a3dcba66471e40d6
二当家小D <xdclass@xdclass.local>
2020-10-22 21:41:47 +0800

    1

---
+++ interview/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println( 1&1 );
+
+        System.out.println( 1&0 );
+
+        System.out.println( 0&1 );
+
+        System.out.println( 0&0 );
+
+    }
+}
